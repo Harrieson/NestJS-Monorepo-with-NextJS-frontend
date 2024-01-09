3e4c803859324d2c48fcd9e3684c33988c2cb4a3
Harrie_Mburu <40524846+Harrieson@users.noreply.github.com>
2024-01-09 14:26:01 +0300

    Adding NX

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,8 +1,10 @@
+import { add } from '@foundation/sample-lib';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  console.log('add', add(3, 35));
   await app.listen(4005);
 }
 bootstrap();
--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,8 +1,7 @@
+import { add } from "@foundation/sample-lib";
 
 export default function Home() {
   return (
-    
-    
-    <main>Hello WOrld</main>
+    <main>Hello WOrld {add(2, 32)}</main>
   )
 }
